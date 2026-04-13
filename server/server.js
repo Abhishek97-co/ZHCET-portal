14e056546d81e953292a15509380ab100fe370b8
Abhishek97-co <abhishekgupta908453908453@gmail.com>
2026-04-13 14:16:34 +0530

    fix wildcard route

--- server/server.js
+++ server/server.js
@@ -33,6 +33,6 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
